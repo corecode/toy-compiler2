c5fc886e6218aa3afd02ac26ee2dc67241fc8425
Simon Schubert <2@0x2c.org>
2015-03-12 01:26:06 +0100

    add builtins, conditionals

--- compile.rb
+++ compile.rb
@@ -5,12 +5,41 @@ def unique(name='%_')
   "#{name}#{$counter}"
 end
 
+$temp_counter = 0
+def temporary
+  $temp_counter += 1
+  "%_#$temp_counter"
+end
+
+$builtins = {}
+{:+ => "add",
+ :- => "sub",
+ :* => "mul",
+ :/ => "sdiv"}.each do |sym, inst|
+  $builtins[sym] = ->(a, b) {
+    retval = temporary()
+    puts "#{retval} = #{inst} i32 #{a}, #{b}"
+    retval
+  }
+end
+
+{'='.to_sym => "eq",
+ :not= => "neq",
+ :> => "sgt",
+ :< => "slt"}.each do |sym, cond|
+  $builtins[sym] = ->(a, b) {
+    retval = temporary()
+    puts "#{retval} = icmp #{cond} i32 #{a}, #{b}"
+    retval
+  }
+end
+
 def translate(expr, env)
   case expr
   when Numeric
-    "i32 #{expr}"
+    "#{expr}"
   when Symbol
-    retval = unique()
+    retval = temporary()
     puts "#{retval} = load i32* #{env[expr]}"
     retval
   when Array
@@ -40,7 +69,7 @@ def translate(expr, env)
       body.each do |e|
         retval = translate(e, newenv)
       end
-      puts "ret #{retval}"
+      puts "ret i32 #{retval}"
       puts "}"
     when :let
       varlist = l[1]
@@ -67,11 +96,62 @@ def translate(expr, env)
       val = translate(l[2], env)
       puts "store i32 #{val}, i32* #{var}"
       val
+    when :if
+      cond = translate(l[1], env)
+      true_br = l[2]
+      true_label = unique('true')
+      false_br = l[3]
+      false_label = unique('false')
+      join_label = unique('join')
+      puts "br i1 #{cond}, label %#{true_label}, label %#{false_label}"
+      puts "#{true_label}:"
+      true_val = translate(true_br, env)
+      puts "br label %#{join_label}"
+      puts "#{false_label}:"
+      if false_br
+        false_val = translate(false_br, env)
+      else
+        false_val = "0"
+      end
+      puts "br label %#{join_label}"
+      puts "#{join_label}:"
+      retval = temporary()
+      puts "#{retval} = phi i32 [#{true_val}, %#{true_label}], [#{false_val}, %#{false_label}]"
+      retval
+    when :while
+      cond = l[1]
+      body = l[2..-1]
+
+      in_label = unique('in')
+      top_label = unique('top')
+      test_label = unique('test')
+      out_label = unique('out')
+      puts "br label %#{in_label}"
+      puts "#{in_label}:"
+      puts "br label %#{test_label}"
+      puts "#{top_label}:"
+      bodyval = "i32 0"
+      body.each do |e|
+        bodyval = translate(e, env)
+      end
+      puts "br label %#{test_label}"
+      puts "#{test_label}:"
+      retval = temporary()
+      puts "#{retval} = phi i32 [0, %#{in_label}], [#{bodyval}, %#{top_label}]"
+      cond = translate(cond, env)
+      puts "br i1 #{cond}, label %#{top_label}, label %#{out_label}"
+      puts "#{out_label}:"
+      retval
     else
       args = l[1..-1].map{|e| translate(e, env)}
-      retval = unique()
-      puts "#{retval} = call i32 #{pred}(#{args.join(", ")})"
-      retval
+      if $builtins.include? pred
+        $builtins[pred].(*args)
+      else
+        retval = temporary()
+        argnames = args.map{|a| "i32 #{a}"}
+        puts "#{retval} = call i32 @#{pred}(#{argnames.join(", ")})"
+        retval
+      end
     end
   end
 end
